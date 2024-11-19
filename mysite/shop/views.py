4fe8de52dbbfeb76d7ec644c61feebb5b0bf78b6
Apizh <aptizh@gmail.com>
2024-11-19 21:50:46 +0400

    Started working with HTML and added the first page

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpRequest #, HttpResponse
 from django.shortcuts import render
 
 
+# def shop_index(request: HttpRequest):
+#     print(request.method)
+#     print(request.path)
+#     print(request.headers)
+#     return HttpResponse("<h1>Hello world, было создано благодаря добавлению приложения в settings, в корневом проекте, добавлением include обработке маршрутов в urls.py, созданию</h1>")
+
 def shop_index(request: HttpRequest):
-    print(request.method)
-    print(request.path)
-    print(request.headers)
-    return HttpResponse("<h1>Hello world, было создано благодаря добавлению приложения в settings, в корневом проекте, добавлением include обработке маршрутов в urls.py, созданию</h1>")
+    return render(request, 'shop/shop-index.html')
