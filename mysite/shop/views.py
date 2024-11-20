8a49c7f0be47e3084e111ce9205c5c367e446919
Apizh <aptizh@gmail.com>
2024-11-20 18:45:51 +0400

    Added processing of the passed array and outputting it as a numbered list and setting the default property. Time output.

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -9,7 +9,22 @@ from timeit import default_timer
 #     return HttpResponse("<h1>Hello world, было создано благодаря добавлению приложения в settings, в корневом проекте, добавлением include обработке маршрутов в urls.py, созданию</h1>")
 
 def shop_index(request: HttpRequest):
+    data = [
+        ('Apple', 1.2),
+        ('Banana', 0.8),
+        ('Orange', 1.5),
+        ('Grape', 0.9),
+        ('Pear', 1.1),
+        ('Cherry', 1.3),
+        ('Melon', 1.4),
+        ('Pineapple', 1.7),
+        ('Mango', 1.6),
+        ('Strawberry', 1.8),
+        ('Blueberry', 1.9),
+        ('Peach', 2.0),
+    ]
     context = {
+        'data' : None,
         'time_running': default_timer(),
     }
     return render(request, 'shop/shop-index.html', context=context)
