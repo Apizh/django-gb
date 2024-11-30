930768749ffc9aac82a303395617ca10e716bd08
apizh <aptizh@gmail.com>
2024-12-01 01:14:00 +0400

    "Create model, migration DB for shop. Add table shop_product in DB"

--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)  # Название продукта, с максимальной длиной в 100 символов
+    description = models.TextField(null=False, blank=True)  # Описание продукта
