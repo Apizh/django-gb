9729d367c9ac2a45835ac923df39f9eaee6eb669
apizh <aptizh@gmail.com>
2024-12-04 21:10:00 +0400

    Added search functionality and defined search fields in admin panel.

--- mysite/shop/admin.py
+++ mysite/shop/admin.py
@@ -12,8 +12,10 @@ class ProductAdmin(admin.ModelAdmin):
                     'created_dat']  # Список полей выводимых в административную панель.
     list_display_links = ['pk',
                           'name']  # Поля, которые будут использоваться для создания ссылки в административной панели.
+    ordering = ['pk', 'price', 'created_dat', 'name']  # Порядок сортировки в административной панели умолчанию.
+    search_fields = 'name', 'description'  # Поля, по которым можно осуществлять поиск в административной панели.'
 
     def description_repr(self, obj: Product) -> str:
-        return obj.description[:50] # переопределение вывода поля description в административной панели.
+        return obj.description[:50]  # переопределение вывода поля description в административной панели.
 
     # admin.site.register(Product, ProductAdmin)  # Вариант опеределения модели Product в административной панели.
